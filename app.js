de2adfb0d5f5d5ad3f1cd46317cccf53cc214542
Iman Ghajar <iman.ghajar89@gmail.com>
2018-05-16 19:11:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var app = angular.module("App", ["ngRoute", "firebase"]);
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'FirstController',
+    })
+    .when('/sign-up', {
+        templateUrl: 'templates/sign-up.html',
+        controller: 'SignUpController'
+    })
+    .when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+    });
+});
